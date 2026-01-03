8963e858571b525707caee67a1a704c16676dbaa
Diana <mikoli@MacBook-Air-Diana.local>
2026-01-03 10:27:55 +0100

    Edit communication front back

--- frontend/gallery-of-art-ui/vite.config.js
+++ frontend/gallery-of-art-ui/vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig({
-  plugins: [react()],
+export default {
   server: {
+    host: true,
+    port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:5212",
+        target: "http://api:8080",
         changeOrigin: true,
-        secure: false
-      }
-    }
-  }
-});
+      },
+    },
+  },
+}
